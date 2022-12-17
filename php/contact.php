b79ba393ecebd5aa8d51d07082c9a05738064ee0
Bawer farhad <81622206+bawerfarhad@users.noreply.github.com>
2022-12-17 08:47:32 +0300

    After Some Changes in frontend layouts

---
+++ php/contact.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['subject']) && isset($_POST['message']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+
+  $test = "/(content-type|bcc:|cc:|to:)/i";
+  foreach ( $_POST as $key => $val ) {
+    if ( preg_match( $test, $val ) ) {
+      exit;
+    }
+  }
+
+$headers = 'From: ' . $_POST["name"] . '<' . $_POST["email"] . '>' . "\r\n" .
+    'Reply-To: ' . $_POST["email"] . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+  mail( "bfh529@cs.soran.edu.iq", $_POST['subject'], $_POST['message'], $headers );
+}
+?>
